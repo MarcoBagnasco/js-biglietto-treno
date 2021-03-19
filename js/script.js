56beb67df6b77f6afa6833103d149dba8a9448a5
MarcoBagnasco <bagna.hbk@gmail.com>
2021-03-19 15:12:58 +0100

    add first task

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-console.log('Hello');
+// console.log('Hello'); //test
+
+
+/*  CALCOLO PREZZO BIGLIETTO
+
+    1.Chiedere i km da percorrere
+    2.Calcolare il prezzo
+    3.Chiedere l'età
+    4.Aggiungere eventuali sconti
+    5.Visualizzare prezzo finale
+*/
+
+// 1.
+var km = parseInt(prompt('Quanti km intendi percorrere?'));
+console.log(km); //test
