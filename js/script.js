6610722251ebe7000dc8c6388a6d19264b3d46ff
MarcoBagnasco <bagna.hbk@gmail.com>
2021-03-19 17:57:20 +0100

    add style and change age calculator

--- js/script.js
+++ js/script.js
@@ -11,18 +11,38 @@
 */
 
 // 1.
+alert('Benvenuto!\nProcediamo con l\'acquisto del biglietto...');
 var km = parseInt(prompt('Quanti km intendi percorrere?'));
-console.log(km); //test
+// console.log(km); //test
 
 // 2.
 var pricePerKm = 0.21;
 var price = km * pricePerKm;
-console.log(price); //test
+// console.log(price); //test
 
 // 3.
-var age = parseInt(prompt('Quanti hanni hai?'));
-console.log(age); //test
+var age;
+var happyBirthday = false;
+var today = new Date();
+var birthDay = parseInt(prompt('Inserisci la tua data di nascita nel formato gg/mm/aaaa.\nGiorno: '));
+var birthMonth = parseInt(prompt('Inserisci la tua data di nascita nel formato gg/mm/aaaa.\nMese: '));
+var birthYear = parseInt(prompt('Inserisci la tua data di nascita nel formato gg/mm/aaaa.\nAnno: '));
+// console.log(birthDay, birthMonth, birthYear); //test
+if( ( today.getMonth() + 1) > birthMonth ){
+    age = today.getFullYear() - birthYear;
+} else if ( ( (today.getMonth() + 1) == birthMonth ) && ( today.getDate() >= birthDay ) ){
+    age = today.getFullYear() - birthYear;
+    
+    if ( today.getDate() == birthDay ){
+    happyBirthday = true;
+    }   
 
+} else {
+    age = (today.getFullYear() - birthYear) - 1;
+}
+
+// console.log(age); //test
+// console.log(happyBirthday); //test
 // 4.
 var juniorDiscount = 0.8;
 var seniorDiscount = 0.6;
@@ -32,9 +52,13 @@ if(age < 18){
 } else if(age > 65){
     price *= seniorDiscount;
 }
-console.log(price); //test
+// console.log(price); //test
 
 // 5.
 var priceString = '€' + price.toFixed(2)
 
-document.getElementById('price').innerHTML = 'Il prezzo del tuo bigietto è ' + priceString;
+if( !happyBirthday){
+    document.getElementById('price').innerHTML = 'Il prezzo del tuo biglietto è ' + priceString;
+} else {
+    document.getElementById('price').innerHTML = 'Oggi è il tuo compleanno e viaggi GRATIS!<br/>Tanti Auguri!!!!';
+}
